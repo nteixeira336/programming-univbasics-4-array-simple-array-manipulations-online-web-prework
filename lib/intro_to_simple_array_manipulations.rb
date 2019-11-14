aa8d9c6c2bc2d35ed29362b50dd121160ecd7a09
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-11-14 15:57:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,7 @@ def "using_push"
   colors_in_the_rainbow = ["red","orange","yellow","green","blue"]
 colors_in_the_rainbow.push ("violet")
 end 
+
+def "using_unshift"
+  colors_in_the_rainbow.unshift ("pink")
+end 
