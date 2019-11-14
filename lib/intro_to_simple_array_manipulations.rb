7b27300bc62ea06f438cd5acc20c27d4acb0d662
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-11-14 15:00:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
 def using_push= colors_in_the_rainbow= ["red","orange","yellow","green","blue"]
 next_color= "violet"
-colors_in_the_rainbow.push(next_color)
+using_push=colors_in_the_rainbow.push(next_color)
 end 
